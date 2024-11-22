6b6432d774b3fa39317d7f504c98cc14dda2d679
k-mendoza-ramirez <kevin5550@live.com>
2024-11-21 22:25:49 -0800

    Fixed minor spelling and grammar issues in about me tab

--- src/About.js
+++ src/About.js
@@ -10,10 +10,10 @@ export default function About() {
                     As a developer, I thrive in environments that challenge me to grow, whether it's by exploring new
                     technologies or solving complex problems creatively. I'm particularly interested in web development and
                     enjoy creating efficient, user-centered solutions. I'm always eager to take on opportunities that allow me to learn quickly,
-                     contribute meaningfully, and collaborate with a team to bring dideas to life.
+                     contribute meaningfully, and collaborate with a team to bring ideas to life.
 
                     While my journey as a developer is just beginning, my time as a server (the restaurant kind!) has equipped me with resilience, teamwork,
-                    and a service-oriented mindset-qualities that I'm excited to carry into the tech world. When I'm not coding,
+                    and a service-oriented mindset; qualities that I'm excited to carry into the tech world. When I'm not coding,
                      you can find me exploring new ways to push the boundaries of electronic music production, constantly honing my skills in sound design and composition.
                 </p>
             </div>
